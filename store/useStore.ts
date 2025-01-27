0a1721956c3a6a6fec38caa777d34010855a3d9a
Femi Abimbola <femiabimbola6@gmail.com>
2025-01-27 15:22:51 +0100

    added store

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,2 +1,9 @@
 import { create } from "zustand";
 import { PersonalInfoSlice } from "./slices/personalInfo";
+import createPersonalInfoSlice from "./slices/personalInfo";
+
+const useStore = create<PersonalInfoSlice>((...a) => ({
+  ...createPersonalInfoSlice(...a),
+}));
+
+export default useStore;
