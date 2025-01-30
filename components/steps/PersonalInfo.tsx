ef2641e689e55b0e348ac1124c4fe4fee92cce32
Femi Abimbola <femiabimbola6@gmail.com>
2025-01-30 17:02:24 +0100

    working on personalInfo

--- components/steps/PersonalInfo.tsx
+++ components/steps/PersonalInfo.tsx
@@ -1,3 +1,40 @@
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import useStore from "@/store/useStore";
+
+const formSchema = z.object({
+  name: z.string().min(5, { message: "Name is required" }).max(100),
+  email: z.string().min(1, { message: "Email is required" }).email({
+    message: "Must be a valid email",
+  }),
+  phone: z.string().min(8, { message: "Enter your phone" }),
+  // phone: z.string().refine((val) => /^\d{10}$/.test(val), {
+  //   message: "Phone is required",
+  // }),
+});
+
+type ValidationSchema = z.infer<typeof formSchema>;
+
 export const PersonalInfo = () => {
-  return <h1> The Personal info</h1>;
+  const { personalInfo, setPersonalInfo, increaseStep } = useStore((state) => state);
+
+  const onSubmitHandler = (values: ValidationSchema) => {
+    setPersonalInfo({ ...personalInfo, ...values });
+    increaseStep(1);
+  };
+
+  const form = useForm<ValidationSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { ...personalInfo },
+  });
+
+  return (
+    <section>
+      The Personal info
+      <button onClick={() => increaseStep(1)}> next step </button>
+    </section>
+  );
 };
