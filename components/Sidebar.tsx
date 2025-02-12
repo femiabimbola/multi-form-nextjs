3fff230340a624b3695a18946cbec18f001a6663
Femi Abimbola <femiabimbola6@gmail.com>
2025-02-12 15:56:25 +0100

    working on the component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,20 @@
+import Pointer from "./Pointer";
+
 const Sidebar = () => {
-  return <p> Sidebar</p>;
+  return (
+    <aside className="hidden lg:flex lg:w-[274px] lg:h-[568px] lg:flex-col lg:flex-shrink-0 rounded-lg lg:bg-[url('/images/bg-sidebar-desktop.svg')] lg:px-8 pt-10 lg:gap-8">
+      <Pointer
+        stepNumber={1}
+        smallTitle="Step 1"
+        sectionTitle="Your Info"
+      />
+      <Pointer
+        stepNumber={2}
+        smallTitle="Step 2"
+        sectionTitle="Select Plan"
+      />
+    </aside>
+  );
 };
 
 export default Sidebar;
